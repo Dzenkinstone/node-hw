eee494306c818d63891b8aad80716e5e6269debe
Dzenkinstone <kozlovdanil6@gmail.com>
2023-04-18 21:22:02 +0200

    changes

--- controllers/getControllers.js
+++ controllers/getControllers.js
@@ -1,18 +1,15 @@
-const {
-  listContacts,
-  getById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../models/contacts");
+const { Contact } = require("../models/contacts");
+
+const { controlWrapper } = require("../helpers/controlWrapper");
 
 const getController = async (req, res, next) => {
-  const contacts = await listContacts();
+  const contacts = await Contact.find();
   res.json(contacts);
 };
 
 const getByIdController = async (req, res, next) => {
-  const findContact = await getById(req.params);
+  const { contactId } = req.params;
+  const findContact = await Contact.findById(contactId);
 
   if (!findContact) {
     return res.json(404, { message: "Not Found" });
@@ -21,13 +18,15 @@ const getByIdController = async (req, res, next) => {
 };
 
 const postController = async (req, res, next) => {
-  const newContact = await addContact(req.body);
+  const newContact = await Contact.create(req.body);
 
   res.json(201, newContact);
 };
 
 const deleteController = async (req, res, next) => {
-  const deleteContact = await removeContact(req.params);
+  const { contactId } = req.params;
+
+  const deleteContact = await Contact.findByIdAndDelete(contactId);
 
   if (!deleteContact) {
     return res.json(404, { message: "Not found" });
@@ -37,7 +36,30 @@ const deleteController = async (req, res, next) => {
 };
 
 const putController = async (req, res, next) => {
-  const replaceContact = await updateContact(req.params, req.body);
+  const { contactId } = req.params;
+  const { name, phone, email } = req.body;
+
+  const replaceContact = await Contact.findByIdAndUpdate(
+    contactId,
+    {
+      $set: { name, phone, email },
+    },
+    { new: true }
+  );
+
+  if (!replaceContact) {
+    return res.json(404, { message: "Not found" });
+  }
+
+  res.json(200, replaceContact);
+};
+
+const patchController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const replaceContact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
 
   if (!replaceContact) {
     return res.json(404, { message: "Not found" });
@@ -47,9 +69,10 @@ const putController = async (req, res, next) => {
 };
 
 module.exports = {
-  getController,
-  getByIdController,
-  postController,
-  deleteController,
-  putController,
+  getController: controlWrapper(getController),
+  getByIdController: controlWrapper(getByIdController),
+  postController: controlWrapper(postController),
+  deleteController: controlWrapper(deleteController),
+  putController: controlWrapper(putController),
+  patchController: controlWrapper(patchController),
 };
